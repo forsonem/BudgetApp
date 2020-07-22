06a4acb01391e816abb4cfafdc69675325c605f6
forsonem <forsonem@gmail.com>
2020-07-22 09:15:14 -0400

    function constructors

--- app.js
+++ app.js
@@ -4,8 +4,57 @@
 
 //BUDGET CONTROLLER
 var budgetController = (function() {
+    //expense function constructor
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    //income function constructor
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    //Budget controller keeps track of incomes and expenses and there totals.
+    var data = {
+       allItems : {
+           exp: [],
+           inc: []
+       },
+       totals : {
+            exp : 0,
+            inc : 0
+       }
+    };
 
-    //Some code
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;    
+
+            //create new ID
+            if (data.allItems[type].length > 0) {
+
+                ID = data.allItems[type][data.allItems[type]. length - 1].id + 1;
+            } else{
+                ID = 0;
+            }
+
+
+            //Create new item depending on type
+            if (type ==='exp'){
+                newItem =  new Expense(ID, des, val);
+            }else if (type === 'inc'){
+                newItem = new Income(ID, des, val);
+            }
+            //push our new item to our array data structure
+            data.allItems[type].push(newItem);
+
+            //return the new element
+            return newItem;
+           
+        }
+    };
 
 })();
 
@@ -27,6 +76,23 @@ var UIController = (function(){
                 value: document.querySelector(Domstrings.inputValue).value
             };
             
+        },
+        //passing in the new item object
+        addListitem: function(obj, type){
+            var html, newHtml;
+            //create html string with place holder text
+            if (type = 'inc'){
+                html = '<div class="item clearfix" id="%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+            }else if (type === 'exp'){
+                html = '<div class="item clearfix" id="%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+            }
+            
+            //replace html string with actual data from the object
+            newHtml = html.replace('%id%', obj.id,);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            //insert the HTML into the DOM
+
         },
         getDomstrings: function() {
             return Domstrings;
@@ -55,11 +121,13 @@ var appController = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function(){
+        var input, newItem;
         //1. Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         console.log(input);
 
         //2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.descripton, input.value);
 
         //3. Add the item to the UI
 
